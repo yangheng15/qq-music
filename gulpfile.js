643239c4b84e8419700d7a939a8b3bbe113b37f5
1220626014@qq.com <wuyudi0212>
2019-11-19 18:22:06 +0800

    '增加了样式'

--- gulpfile.js
+++ gulpfile.js
@@ -17,41 +17,59 @@ var cleanCss = require('gulp-clean-css');
 var postCss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 // 开启服务器 gulp-connect-reproxy
-var server = require('gulp-connect-reproxy')
+var connect = require('gulp-connect');
+
+//判断环境变量，如果是开发环境不压缩文件
+var devMod = process.env.NODE_ENV === 'development';
+// set NODE_ENV=development 设置环境变量
+console.log( process.env.NODE_ENV);
+console.log(devMod);
 var floder = {
     src: 'src/',
     dist: 'dist/'
 };
 
 gulp.task('html', function (cd) {
-    gulp.src(floder.src + 'html/*.html')
-        .pipe(htmlClean())
+    var page = gulp.src(floder.src + 'html/*.html');
+        if(!devMod) {
+            console.log('执行了压缩html')
+            page.pipe(htmlClean())
+        }
+        page.pipe(connect.reload())
         .pipe(gulp.dest(floder.dist + 'html/'))
     cd();
 })
 
 gulp.task('css', function (cd) {
-    gulp.src(floder.src + 'css/*.less')
+    var page = gulp.src(floder.src + 'css/*.less')
         .pipe(trunLess()) // less转化css
         .pipe(postCss([autoprefixer])) // 自动补前缀
-        .pipe(cleanCss()) // 压缩
+        if(!devMod) {
+            page.pipe(cleanCss()) // 压缩
+        }
+        page.pipe(connect.reload())
         .pipe(gulp.dest(floder.dist + 'css/'))
     cd();
 })
 
 gulp.task('js', function (cd) {
-    gulp.src(floder.src + 'js/*.js')
+    var page = gulp.src(floder.src + 'js/*.js')
         .pipe(stripDebug())
-        .pipe(uglify()) // 压缩js
+        if(!devMod) {
+            page.pipe(uglify()) // 压缩js
+        }
+        page.pipe(connect.reload())
         .pipe(gulp.dest(floder.dist + 'js/'))
     cd();
 })
 
 //图片需要压缩，用到插件时npm i gulp-imagemin
 gulp.task('image', function (cd) {
-    gulp.src(floder.src + 'image/*')
-        .pipe(imageMin())
-        .pipe(gulp.dest(floder.dist + 'image/'))
+    var page = gulp.src(floder.src + 'image/*')
+        if(!devMod) {
+            page.pipe(imageMin()) // 压缩图片
+        }
+        page.pipe(gulp.dest(floder.dist + 'image/'))
     cd();
 })
 
@@ -61,7 +79,23 @@ gulp.task('image', function (cd) {
 //
 // });
 
+gulp.task('server', function (cd) {
+    connect.server({
+        port: '12306',
+        livereload: true
+    }); // 开启一个服务
+    cd();
+})
+
+gulp.task('watch', function (cd) {
+    gulp.watch(floder.src + 'html/*', gulp.series('html'));
+    gulp.watch(floder.src + 'css/*',gulp.series('css'));
+    gulp.watch(floder.src + 'js/*',gulp.series('js'));
+    cd()
+})
 // gulp 4.0 执行方式 需要使用 gulp.series 或者 使用 gulp.parallel
-gulp.task("default", gulp.series('html', 'css', 'js', 'image',  function () {
+gulp.task("default", gulp.series('html', 'css', 'js', 'image', 'server', 'watch',  function () {
 
 }))
+
+
